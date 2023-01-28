aab3b8aa2e2e3e39e9713980f0f2bfdb8858af4b
Zulk0rne1n <ulasardaburak@outlook.com>
2023-01-29 02:27:27 +0300

    Exercise11 added

---
+++ Exercise11.js
@@ -0,0 +1,17 @@
+// Modify the second term of the expression in order to get true as result.
+
+// 10 < 9
+// 45 == 34
+// true === 1
+// false === 0
+// 1 == 0
+// 4 <= 2
+// 4 >= 8
+
+10 < 11
+45 == 45
+true === true
+false === false
+1 == 1 // "1", true
+4 <= 4 // 5,6,7,8...
+4 >= 3 // 2,1,0
