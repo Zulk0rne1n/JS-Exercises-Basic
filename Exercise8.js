90b71a8fa4f37f7799385658c5c75ef5d83b74a6
Zulk0rne1n <ulasardaburak@outlook.com>
2023-01-29 01:18:05 +0300

    Exercise8 added

---
+++ Exercise8.js
@@ -0,0 +1,18 @@
+let isValid = true;
+console.log(isValid, typeof(isValid));
+
+// number
+isValid = Number(isValid);
+console.log("converted to number");
+console.log(isValid, typeof(isValid));
+
+// string
+isValid = String(isValid);
+console.log("converted to string");
+console.log(isValid, typeof(isValid));
+
+// boolean
+isValid = Boolean(isValid);
+console.log("converted to boolean");
+console.log(isValid, typeof(isValid));
+
