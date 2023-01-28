3207dda2587df88506f2fdc854e3451d306bae11
Zulk0rne1n <ulasardaburak@outlook.com>
2023-01-29 02:21:49 +0300

    Exercise10 added

---
+++ Exercise10.js
@@ -0,0 +1,5 @@
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+yearsCompleted == firstYearCompleted  // Double equals checks if the values are equal
+yearsCompleted === firstYearCompleted // Triple equals check if the values are equal and also if the variable types are the same
