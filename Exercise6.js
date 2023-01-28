aefaea25606f3dbba01dc7929b1b0670780c1d7a
Zulk0rne1n <ulasardaburak@outlook.com>
2023-01-29 00:35:14 +0300

    Exercise 6 added

---
+++ Exercise6.js
@@ -0,0 +1,12 @@
+let hello = 'Ciao';     // boolean
+let age = 18;       // string
+let isGraduated = false;     // number
+
+Boolean(hello);
+String(age);
+Number(isGraduated);
+
+//if we need to print those values
+console.log(Boolean(hello));
+console.log(String(age));
+console.log(Number(isGraduated));
