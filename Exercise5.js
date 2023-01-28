eb3825e65a21bf351a97a09526abd3b7d3ce9958
Zulk0rne1n <ulasardaburak@outlook.com>
2023-01-29 00:25:25 +0300

    Exercise5 added

---
+++ Exercise5.js
@@ -0,0 +1,10 @@
+let isDoorClosed;
+isDoorClosed = true;
+let isDogOutside;
+isDogOutside = false;
+
+if (isDoorClosed == true && isDogOutside == false) {
+    console.log("the door is closed and the dog is inside");
+  } else if (isDoorClosed == false && isDogOutside == true) {
+    console.log("The door is open and the dog is outside");
+  } else console.log("Modify the variables values");
