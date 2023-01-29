c08648713b9f276b5fe0e908ac97b365fc4aa760
Zulk0rne1n <ulasardaburak@outlook.com>
2023-01-29 04:12:51 +0300

    Exercise17 added

---
+++ Exercise17.js
@@ -0,0 +1,11 @@
+let isSunnyDay = true;
+let isHomeWorkCompleted = true;
+if(isSunnyDay && isHomeWorkCompleted) {
+    
+console.log("Jesse can go out to play")
+} else {
+
+
+console.log("Jesse stays home")
+}
+
