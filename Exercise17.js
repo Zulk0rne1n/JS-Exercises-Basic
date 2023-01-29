c0b6d5f381c56b333ca5721de56b03289e8a46fc
Zulk0rne1n <ulasardaburak@outlook.com>
2023-01-29 04:17:58 +0300

    Exercise17 added

---
+++ Exercise17.js
@@ -0,0 +1,10 @@
+let age = 29;
+let legalAge = 18;
+str = "";
+if(age >= legalAge) {
+    str = "You can drive a car!"
+} else {
+    str = "You are too young to drive a car!"
+}
+
+console.log(str)
