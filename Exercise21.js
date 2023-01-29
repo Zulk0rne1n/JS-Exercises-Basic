a5176d59262b07ba8c02012376ccca31b7bf7c2b
Zulk0rne1n <ulasardaburak@outlook.com>
2023-01-29 05:33:53 +0300

    added an alternative way in comments

--- Exercise21.js
+++ Exercise21.js
@@ -1,6 +1,8 @@
 let password = "asd12";
 let len = password.length;
 let systemMessage = "";
+//or
+// let systemMessage = len >=4 && len <=6 ? "Password length is correct" : "Password not valid";
 
 if (len >= 4 && len <=6) {
     systemMessage = "Password length is correct";
