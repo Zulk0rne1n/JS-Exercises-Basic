b9723a740c199d455159e798a5fffb9813a4003b
Zulk0rne1n <ulasardaburak@outlook.com>
2023-02-02 00:31:59 +0300

    Exercise36 added

---
+++ Exercise36.js
@@ -0,0 +1,13 @@
+function canPlay() {
+    var sport = " Football";
+    
+    if (true) {
+    var personName = "Cosimo";
+    }
+    
+    console.log(personName + sport);
+    }
+    
+    canPlay();
+
+    // We can use var instead of let while defining our variables.
