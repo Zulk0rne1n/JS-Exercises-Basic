a802a010b4d27460da07886cbf7ee23dac4fb79d
Zulk0rne1n <ulasardaburak@outlook.com>
2023-02-02 00:29:56 +0300

    Exercise35 updated

--- Exercise35.js
+++ Exercise35.js
@@ -3,7 +3,9 @@
 //     if (true) { 
 //         let personName = "Cosimo";
 //     }
-//     console.log(personName + sport); || Function's scope cannot access this line of code because personName is defined within the if block's scope
+//     console.log(personName + sport); || Function's scope cannot access this line of code because personName is defined within the if block's scope.
+//                                          one way to solve this is to put this line inside the if statement 
+//                                          or we can use `var` keyword instead of `let` inside the if statement so it can be accesible outside of its scope
 // }
 
 // canPlay()
@@ -13,7 +15,7 @@ function canPlay() {
     let sport = " Football";
     if (true) { 
         let personName = "Cosimo";
-    console.log(personName + sport);
+        console.log(personName + sport);
     }
 }
 canPlay();
