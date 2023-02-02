826173004f324c6732dfd916366f765f3ef14090
Zulk0rne1n <ulasardaburak@outlook.com>
2023-02-03 00:11:02 +0300

    Exercise46 added

---
+++ Exercise46.js
@@ -0,0 +1,12 @@
+// We want to add to the student object a nested object called personalData,
+//     that has 3 properties: name, surname, age. Print in the console the personalData object.
+
+const student = {
+    personalData : {
+        id: 1,
+        school: "Liceo",
+        year: 3,
+    }
+};
+
+console.log(student.personalData);
