8a0054011a8e3d0c629ec14fabf437fe0280117e
Zulk0rne1n <ulasardaburak@outlook.com>
2023-02-02 05:35:09 +0300

    Exercise39 added

--- Exercise39.js
+++ Exercise39.js
@@ -11,6 +11,5 @@ console.log(multiplyByTwo(5));
 
 // let value = 3;
 // let result = () => console.log(multiplyByTwo(value));
-
 // result();
 // I've commented out added this part for future ref. //
