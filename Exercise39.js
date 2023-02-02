5adacb530227c3355349c4027158c52f04e6673b
Zulk0rne1n <ulasardaburak@outlook.com>
2023-02-02 05:29:56 +0300

    Exercise39 added

---
+++ Exercise39.js
@@ -0,0 +1,16 @@
+function multiplyByTwo(value) {
+    function inner() {
+        let number = 2;
+        return(value * number);
+    }
+    return inner();
+}
+
+console.log(multiplyByTwo(5));
+
+
+// let value = 3;
+// let result = () => console.log(multiplyByTwo(value));
+
+// result();
+// I've commented out added this part for future ref. //
