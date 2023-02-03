efc9b5c2b70697a37a47c046394605ff3950aefe
Zulk0rne1n <ulasardaburak@outlook.com>
2023-02-03 04:33:07 +0300

    Exercise50 added

---
+++ Exercise50.js
@@ -0,0 +1,7 @@
+const names = ["Luca", "Marco", "Vittorio", "Giovanni"];
+
+names.pop(); // Remove the element Giovanni.
+names.push("Pippo"); // Add a new element called Pippo at the end of the array.
+names.unshift("Giovanni") // Add the element Giovanni to the beginning of the array.
+
+console.log(names);
