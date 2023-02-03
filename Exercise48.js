3aadfadcef6bf01bd18d061f6df12dc5dd9d34ae
Zulk0rne1n <ulasardaburak@outlook.com>
2023-02-03 03:57:29 +0300

    Exercise48 added

---
+++ Exercise48.js
@@ -0,0 +1,24 @@
+let students = [
+    student1 = {
+        id : 1,
+        name : "Rose",
+        surname : "O'Sharyn",
+        age : 10,
+    },
+    student2 = {
+        id : 2, 
+        name : "Bucket", 
+        surname : "Head",
+        age : 20,
+    },
+    student3 = {
+        id : 003,
+        name : "Gun'N",
+        surname : "Roses",
+        age : 30,
+    }
+
+
+]
+
+console.log(students[0]);
